1a42c1a56126f0abff15a14a3f602dca6e61f875
Elisa <elisat15@uw.edu>
2019-10-18 15:41:25 -0700

    Updates

---
+++ precache-manifest.58b11b0b639cc4c62d77df040a36801c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a58ec0bae707e21ef70d287a17a4023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed2eec498045bbb317a3",
+    "url": "/static/css/main.98efbfb6.chunk.css"
+  },
+  {
+    "revision": "14b6ee6ab92e90b65a2b",
+    "url": "/static/js/2.56067e7a.chunk.js"
+  },
+  {
+    "revision": "ed2eec498045bbb317a3",
+    "url": "/static/js/main.0f49b893.chunk.js"
+  },
+  {
+    "revision": "5a8b600985f239a57f6e",
+    "url": "/static/js/runtime-main.ed01415e.js"
+  }
+]);
